d3028f7240c406822fb4198ec69dae9e374be33f
andrey shalaev <ivel.sha@gmail.com>
2015-10-26 11:24:26 +0300

    html2js and copy

--- gruntfile.js
+++ gruntfile.js
@@ -2,54 +2,87 @@
 
 module.exports = function (grunt) {
 
-  grunt.initConfig({
-      browserify: {
-        app: {
-          // A single entry point for our app
-          src: [
-                './src/app/js/modules/foo/foo.js',
-                './src/app/js/modules/bar/bar.js',
-                './src/app/js/index_controller.js',
-                './src/app/js/app.js',
+    grunt.initConfig({
+        browserify: {
+            app: {
+              // A single entry point for our app
+                src: [
+                    './src/app/js/modules/foo/foo.js',
+                    './src/app/js/modules/bar/bar.js',
+                    './src/app/js/index_controller.js',
+                    './src/app/js/app.js',
+                    './src/app/dest/js/templates.js',
+                    ],
+                // Compile to a single file to add a script tag for in your HTML
+                dest: './src/app/dest/js/app.js',
+            }
+        },
+        jshint: {
+            files: [
+                'package.json',
+                'gruntfile.js', 
+                './src/app/js/*.js', 
+                './src/app/js/modules/*/**.js', 
+                './src/app/js/common/*/**.js',
                 ],
-          // Compile to a single file to add a script tag for in your HTML
-          dest: './src/app/dest/js/app.js',
-        }
-      },
-      jshint: {
-        files: [
-            'package.json',
-            'gruntfile.js', 
-            './src/app/js/*.js', 
-            './src/app/js/modules/*/**.js', 
-            './src/app/js/common/*/**.js'
-            ],
-        options: {
-          jshintrc: ".jshintrc"
-        }
-      },
-      uglify: {
-        my_target: {
-          files: {
-            './src/app/dest/js/app.min.js': ['./src/app/dest/js/app.js']
-          }
-        }
-      },
-      watch: {
-        js: {
-          files: ['<%= jshint.files %>'],
-          tasks: ['jshint', 'browserify:app', 'uglify']
-        }
-      }      
-  });
+            options: {
+                jshintrc: ".jshintrc"
+            }
+        },
+        html2js: {
+            options: {
+              // custom options, see below
+            },
+            main: {
+                src: ['src/app/js/modules/**/*.html'],
+                dest: './src/app/dest/js/templates.js'
+            },
+        },
+        uglify: {
+            my_target: {
+                files: {
+                  'build/js/app.min.js': ['./src/app/dest/js/app.js']
+                }
+            }
+        },
+        copy: {
+            build: {
+                files: [
+                    {
+                        src: [ './src/app/index.html' ],
+                        dest: 'build',
+                        expand: true,
+                        filter: 'isFile',
+                        flatten: true
+                    }
+                ]
+            },
+        },
+        watch: {
+            js: {
+                files: ['<%= jshint.files %>',],
+                tasks: ['jshint', 'browserify:app', 'uglify']
+            },
+            tpls: {
+                files: ['<%= html2js.main.src %>'],
+                tasks: ['html2js', 'browserify', 'uglify']
+            },
+            main_html: {
+                files: ['./src/app/index.html'],
+                tasks: ['copy']
+            }
+        }      
+    });
 
 
-  // Load the npm installed tasks
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+    // Load the npm installed tasks
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ["jshint", "browserify:app", "uglify", "watch"]);
+    grunt.registerTask('default', ["jshint", "html2js", "browserify", "uglify", "copy", "watch"]);
 };
--- src/app/js/app.js
+++ src/app/js/app.js
@@ -5,7 +5,8 @@ var angular = require('angular');
 var app = angular.module('foobar', 
     [ 
         require('./common/common.js').name,
-        require('./modules/modules.js').name
+        require('./modules/modules.js').name,
+        'templates-main'
     ]);
 
 app.config(['$stateProvider', '$urlRouterProvider',
@@ -16,12 +17,12 @@ app.config(['$stateProvider', '$urlRouterProvider',
         $stateProvider
             .state('foo', {
                 url: '/foo',
-                templateUrl: "js/modules/foo/view.html",
+                templateUrl: "app/js/modules/foo/view.html",
                 controller: require('./modules/foo/foo.js'),
             })
             .state('bar', {
                 url: '/bar',
-                templateUrl: "js/modules/bar/view.html",
+                templateUrl: "app/js/modules/bar/view.html",
                 controller: require('./modules/bar/bar.js'),
             });
         }]
